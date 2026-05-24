83ee6d83a699e291fa53152fddae17bd5617c79b
Carlos G. Rodriguez R. <carlos.rodriguez18rr@gmail.com>
2026-05-24 18:42:53 +0100

    CornerShop: Readme file was created

--- CornerShop/src/Main.java
+++ CornerShop/src/Main.java
@@ -28,25 +28,28 @@ class Main{
         double otherExpenses = scanner.nextDouble();
 
         double totalExpenses = staffExpenses + otherExpenses;
+        scanner.close();
 
         return revenue - totalExpenses;
     }
 
     public static void main(String[] args){
-        String[] items = new String[] {"Bubblegum", "Toffee", "Ice cream", "Milk chocolate", "Doughnut", "Pancake"};
-        double[] prices = new double[] {2.0, 0.2, 5.0, 4.0, 2.5, 3.2};
+        final String[] ITEMS = new String[] {"Bubblegum", "Toffee", "Ice cream", "Milk chocolate", "Doughnut", "Pancake"};
+        final double[] PRICES = new double[] {2.0, 0.2, 5.0, 4.0, 2.5, 3.2};
 
 
         // Stage 1: Display items with their respective prices
         System.out.println("Prices:");
-        displayItemsWithMoney(items, prices);
+        displayItemsWithMoney(ITEMS, PRICES);
+
+        System.out.println();
 
         // Stage 2: Display the total revenue
-        double[] monthIncome = new double[] {202, 118, 2250, 1680, 1075, 80};
-        double revenue = calculateTotalRevenue(monthIncome);
+        final double[] MONTH_INCOME = new double[] {202, 118, 2250, 1680, 1075, 80};
+        double revenue = calculateTotalRevenue(MONTH_INCOME);
 
         System.out.println("Earned amount:");
-        displayItemsWithMoney(items, monthIncome);
+        displayItemsWithMoney(ITEMS, MONTH_INCOME);
         System.out.printf("\nIncome: $%.1f\n", revenue);
 
         // Stage 3: Display the net income
